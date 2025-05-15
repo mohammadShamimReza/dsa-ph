f69388f24be3b651e6df8d36890d14010a016c7d
Md Shamim Reza <mohammadshamimreza23393@gmail.com>
2025-05-15 19:49:24 +0800

    solved count before 1

--- assignment_3/Count_Before_One.c
+++ assignment_3/Count_Before_One.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
-int main(){
 
-    
-    return 0;
+int count_before_one(int arr[], int n){
+    int index = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i]== 1){
+           return index = i;
+        }
+    }
+
+    return index;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+   int res =  count_before_one(arr, n);
+
+   printf("%d", res);
+
+   return 0;
 };
