0b59fed127245131139831680655c94b7b266a0f
Md Shamim Reza <mohammadshamimreza23393@gmail.com>
2025-07-08 11:26:31 +0800

    solve another problem

--- Data-structure/assingnment_1/Printing_X.cpp
+++ Data-structure/assingnment_1/Printing_X.cpp
@@ -5,8 +5,39 @@ int main (){
     cin >> n;
     for (int i = 0; i < n/2; i++)
     {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        cout << "\\";
+        for (int k = i; k < n - 2 - i; k++)
+        {
+            cout << " ";
+        }
+        cout << "/";
 
-      
+        cout << endl;
+    }
+    for (int l = 0; l < n / 2; l++)
+    {
+        cout << " ";
+    }
+
+    cout << "X" << endl;
+    for (int i = 0; i < n / 2; i++)
+    {
+        for (int j = 0; j < n / 2 - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << "/";
+        for (int k = 0; k < i * 2 + 1; k++)
+        {
+            cout << " ";
+        }
+        cout << "\\";
+
+        cout << endl;
     }
 
     return 0;
