40c59b8330e320f4d9b9e729744538e31c60e6b8
Md Shamim Reza <mohammadshamimreza23393@gmail.com>
2025-07-11 10:19:24 +0800

    insert any position

---
+++ Data-structure/linkedList/insert_any_position.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_to_any_position(Node *head, int pos, int val)
+{
+    Node *newNode = new Node(val);
+    Node *temp = head;
+    for (int i = 0; i < pos -1; i++)
+    {
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+};
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+
+        temp = temp->next;
+    }
+}
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+
+    head->next = a;
+    a->next = b;
+    b->next = c;
+
+    insert_to_any_position(head, 2, 300);
+    print_linked_list(head);
+
+    return 0;
+}
