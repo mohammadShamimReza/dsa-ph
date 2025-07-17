d62b3bcc0c52153a94d9fdfd03fb377728b28c56
Md Shamim Reza <mohammadshamimreza23393@gmail.com>
2025-07-17 17:24:54 +0800

    solve doubly linked list

---
+++ Data-structure/dubly_linked_list/doubly_linked_list.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+
+    public:
+        int val;
+        Node *next;
+        Node *prev;
+
+        Node(int val)
+        {
+            this->val = val;
+            this->next = NULL;
+            this->prev = NULL;
+        }
+};
+void printDoublyLinkedList(Node *tail) {
+    Node *temp = tail;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->prev ;
+    }
+};
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+    printDoublyLinkedList(tail);
+    return 0;
+}
