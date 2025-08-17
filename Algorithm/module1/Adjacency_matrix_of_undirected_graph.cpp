2a40e82d31922967b577bfb31f2f4349573028ca
Md Shamim Reza <mohammadshamimreza23393@gmail.com>
2025-08-17 20:29:06 +0800

    learnded graph adjacency matrix

---
+++ Algorithm/module1/Adjacency_matrix_of_undirected_graph.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main (){
+  int n, e;
+  cin >> n >> e;
+  int adj_mat [n][n];
+ 
+
+  memset(adj_mat, 0, sizeof(adj_mat));
+
+  while (e--)
+  {
+      int a, b;
+      cin >> a >> b;
+      adj_mat[a][b] = 1;
+      adj_mat[b][a] = 1;
+      adj_mat[e][e] = 1;
+  }
+
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+        cout << adj_mat[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
