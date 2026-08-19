b7ea71cd938e59fca299a0cfcbab15b2a5f1bae9
Md Shamim Reza <mohammadshamimreza23393@gmail.com>
2026-08-19 16:06:09 +0600

    need to work on fence

--- ps/B_Fence.cpp
+++ ps/B_Fence.cpp
@@ -3,6 +3,26 @@ using namespace std;
 
 int main()
 {
+    int n, t;
+    cin >> n >> t;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int minh = 0;
+    int mini = 0;
+    for (int i = 0; i < t; i++)
+    {
+        minh = minh + arr[i];
+    }
 
+    for (int i = t; i < n; i++)
+    {
+        int ch = minh - arr[i - t];
+        minh = ch + arr[i];
+        cout << minh;
+    }
+    // cout << arr[6];
     return 0;
 }
